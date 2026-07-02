112adeacf31b9b58a82a682c5a9c9a05ff1e6724
HoneyLatlll <rlaalstn9415@naver.com>
2026-07-02 15:27:12 +0900

    feat: 회원가입 API 구현

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import "dotenv/config";
+import userRouter from "./routes/user.router.js";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/users", userRouter);
 
 const port = process.env.PORT ?? 3001;
 app.listen(port, () => {
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,29 @@
+import bcrypt from "bcrypt";
+import prisma from "../config/prisma.js";
+
+const createUser = async (req, res, next) => {
+  try {
+    const { email, name, password } = req.body;
+    if (!email || !name || !password)
+      throw new Error("이메일 이름 비밀번호 모두 필요합니다");
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const userData = await prisma.user.create({
+      data: {
+        email,
+        nickname: name,
+        encryptedPassword: hashedPassword,
+      },
+    });
+
+    const { encryptedPassword, ...rest } = userData;
+    const safeUserData = rest;
+
+    res.status(201).json(safeUserData);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+export default createUser;
