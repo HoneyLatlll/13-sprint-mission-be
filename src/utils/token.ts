a9ae8093d91cea0e7f0971c6821b351ff5a44b02
HoneyLatlll <rlaalstn9415@naver.com>
2026-08-10 13:32:58 +0900

    refactor: token.ts로 마이그레이션

---
+++ src/utils/token.ts
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken";
+import { User } from "../generated/prisma/client.js";
+
+const createToken = (user: Pick<User, "id">) => {
+  const payload = { userId: user.id };
+  const token = jwt.sign(payload, process.env.JWT_SECRET!, {
+    expiresIn: "1d",
+  });
+  return token;
+};
+
+export default createToken;
